45baa8a213ccfe2849a6694e594d13b6cbeef593
yuanbaowang <2637562166@qq.com>
2020-01-04 10:27:01 +0800

    添加新的工具类  各种随机

--- src/main/java/yuanbaowang_cms_utils/Cal.java
+++ src/main/java/yuanbaowang_cms_utils/Cal.java
@@ -3,6 +3,9 @@
  */
 package yuanbaowang_cms_utils;
 
+import java.util.Calendar;
+import java.util.Iterator;
+
 /**
  * @author 袁保旺
  *
@@ -31,6 +34,7 @@ public class Cal {
       * @return 返回距离 单位千米
     
       */
+    
     public static double getDistance(double longitude1, double latitude1, double longitude2, double latitude2) {
         // 纬度
         double lat1 = Math.toRadians(latitude1);
@@ -48,6 +52,71 @@ public class Cal {
         // 弧长乘地球半径, 返回单位: 千米
         s = s * EARTH_RADIUS * 1000;
         return s;
+    }
+    
+    
+    /**
+     * 	月考计算经纬度工具类
+     */
+    public static int getKm(double longitude, double latitude) {
+    	double a =  Math.round( Math.sqrt( Math.pow(longitude - 39 ,2  ) +  Math.pow(longitude - 116 ,2  ) ));
+    	if(a >= 0 && a <= 15) {
+    		return 2;
+    	}
+    	
+    	if(a >= 16 && a <= 30) {
+    		return 3;
+    	}
+    	
+    	if(a >= 31 && a <= 40) {
+    		return 4;
+    	}
+    	
+    	if(a >= 41 && a <= 60) {
+    		return 5;
+    	}else {
+    		return 6;
+    	}
+    	
+    }
+    
+    /**
+     * 	月考计算是否违规工具类
+     * 	str 代表时间  car代表车类型，km代表距离 carId,汽车牌照
+     */
+    public static int getRule(String str,String car,String carId,int km) {
+    	if(car.equals("A") && km < 15) {
+    		return 0;//"摩托车A进入2环"
+    	}
+    	
+    	if(car.equals("B") && km < 40) {
+    		return 1;//"摩托车B进入4环"
+    	}
+    	
+    	if(car.equals("C") && km >15 && !carId.substring(0,1).equals("京") && km < 60) {
+    		return 2;//"外地牌照不能进入5环"
+    	}
+    	
+    	return 3;//咩问题
+    }
+    
+    /**
+     * 	单双号限行
+     */
+    public static int getSingleDouble(String str,String car,String carId,int km) {
+    	if(car.equals("C") && km >15 && carId.substring(0,1).equals("京")) {
+    		//计算车牌号最后一位数字
+    		String substring = str.substring(carId.length()-1,carId.length());
+    		int sub = Integer.valueOf(substring);
+    		//找天数
+    		String substring2 = str.substring(8,10);
+    		int sub2 = Integer.valueOf(substring2);
+    		
+    		if(sub2 % sub != 0) {
+    			return 1;
+    		}
+    	}
+    	return 0;
     }
 
 }
